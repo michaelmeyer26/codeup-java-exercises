3b865c75d9e9a87368f242e8bf844145d2ed8264
Michael Meyer <michael.c.meyer26@gmail.com>
2020-09-28 16:53:11 -0500

    completed first java exercise and experimentation

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,5 +3,36 @@ public class HelloWorld {
 //        System.out.println("Hello, World!");
         System.out.print("Hello,");
         System.out.print(" World!");
+        System.out.println();
+
+
+        int myFavoriteNumber = 26;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "Rest in Peace, Edgar.";
+        System.out.println(myString);
+
+        float myNumber = 3.14F;
+        System.out.println(myNumber);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        int x = 4;
+//        x += 5;
+
+
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+
+        int x = 10;
+        int y = 2;
+        x /= y;
+        y -= x;
+
+        System.out.println(x);
+        System.out.println(y);
     }
 }
