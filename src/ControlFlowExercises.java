894eb50dc7e8e13555ca6ba5310093862e7edd08
Michael Meyer <michael.c.meyer26@gmail.com>
2020-10-01 14:04:46 -0500

    FizzBuzz completed

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -9,5 +9,20 @@ public class ControlFlowExercises {
             System.out.println(a);
         }
 
+
+        //FizzBuzz
+        for (int j = 1; j <= 100; j++) {
+            if (j % 3 == 0 & j % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (j % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (j % 5 == 0) {
+                System.out.println("Buzz");
+            }else {
+                System.out.println(j);
+            }
+        }
+
+
     }
 }
