1081a52c048c9afa9130ffa42a05a1f74460ed29
Michael Meyer <michael.c.meyer26@gmail.com>
2020-10-02 15:40:01 -0500

    completed basic arithmetic

---
+++ src/MethodsExercises.java
@@ -0,0 +1,26 @@
+public class MethodsExercises {
+    public static void main(String[] args) {
+        System.out.println(addition(4, 2));
+        System.out.println(subtraction(4, 2));
+        System.out.println(multiplication(4, 2));
+        System.out.println(division(4, 2));
+    }
+
+    public static double addition (double x, double y) {
+        return x + y;
+    }
+
+    public static double subtraction (double x, double y) {
+        return x - y;
+    }
+
+    public static double multiplication (double x, double y) {
+        return x * y;
+    }
+
+    public static double division (double x, double y) {
+        return x / y;
+    }
+
+
+}
